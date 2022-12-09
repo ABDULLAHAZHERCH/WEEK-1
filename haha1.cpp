8d02a4e02b220bb16a97a88f93b61009d84d4bbd
ABDULLAHAZHERCH <117835393+ABDULLAHAZHERCH@users.noreply.github.com>
2022-12-08 22:32:57 -0800

    Add files via upload

---
+++ haha1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main(){
+int n1,n2,n3;
+cout<<"Enter a number :";
+cin>>n1;
+cout<<"Enter 2nd number :";
+cin>>n2;
+n3=n1+n2;
+cout<<"Your ans is .. : "<<n3;
+
+
+
+
+
+
+}
